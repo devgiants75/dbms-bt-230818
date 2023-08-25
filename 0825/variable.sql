3a14b6214bd2ae6024b736151838fb892e5096e6
devgiants75 <devgiants75@naver.com>
2023-08-25 11:49:03 +0900

    20230825

--- 0825/variable.sql
+++ 0825/variable.sql
@@ -15,11 +15,10 @@ set @myVar1 = 5;
 set @myVar2 = 4.25;
 
 select @myVar1;
+# 실수 연산의 경우 오류 발생(자릿수를 증가)이 일어나는 경우가 빈번
 select @myVar1 + @myVar2;
 
 set @txt = '가수 이름 >>>';
 set @height = 166;
+# where height > 166과 동일
 select @txt, mem_name from members where height > @height;
-
-
-
